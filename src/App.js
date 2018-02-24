9b7b980d4df72d7eb75aaf2a4ed96c35bb95985e
Rory Smith <rory.smith@korelogic.co.uk>
2018-02-24 22:22:56 +0000

    Add custom text input

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
+const customTextInput = (props) => {
+  console.log('props: ', props);
+  return (
+    <div>
+      <label htmlFor="name">Name</label>
+      <input {...props.input} type="text" />
+    </div>
+  )
+}
+
+const customSelectInput = (props) => {
+  return (
+    <div>
+      <label htmlFor="preference">Preferred Formatting</label>
+      <select {...props.input} name="preference">
+        <option />
+        <option value="tabs">Tabs</option>
+        <option value="spaces">Spaces</option>
+      </select>
+    </div>
+  )
+}
+
+const customCheckboxInput = (props) => {
+  return (
+    <div>
+      <label htmlFor="terms">Sign up to newsletter?</label>
+      <input {...props.input} name="terms" type="checkbox" />
+    </div>
+  )
+}
+
 class UserForm extends Component {
   render() {
     const { handleSubmit } = this.props;
     return (
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name</label>
-          <Field name="name" component="input" type="text" />
-        </div>
-        <div>
-          <label htmlFor="name">Preferred Formatting</label>
-          <Field name="preference" component="select">
-            <option />
-            <option value="tabs">Tabs</option>
-            <option value="spaces">Spaces</option>
-          </Field>
-        </div>
-        <div>
-          <label htmlFor="terms">Sign up to newsletter?</label>
-          <Field name="termsAccepted" component="input" type="checkbox" />
-        </div>
+        <Field name="name" component={customTextInput} type="text" />
+        <Field name="preference" component={customSelectInput} />
+        <Field name="termsAccepted" component={customCheckboxInput} type="checkbox" />
         <button type="submit">Submit</button>
       </form>
     );
