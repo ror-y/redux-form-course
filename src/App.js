9f33ef031313a648c7d9b5585c534ed96c9a6e58
Rory Smith <rory.smith@korelogic.co.uk>
2018-02-24 22:42:09 +0000

    Simplify bootstrap lesson

--- src/App.js
+++ src/App.js
@@ -1,47 +1,20 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
-class UserForm extends Component {
+class App extends Component {
   render() {
-    const { handleSubmit } = this.props;
     return (
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name</label>
-          <Field name="name" component="input" type="text" />
-        </div>
-        <div>
-          <label htmlFor="preference">Preferred Formatting</label>
-          <Field name="preference" component="select">
-            <option />
-            <option value="tabs">Tabs</option>
-            <option value="spaces">Spaces</option>
-          </Field>
-        </div>
-        <div>
-          <label htmlFor="newsletter">Sign up to newsletter?</label>
-          <Field name="newsletter" component="input" type="checkbox" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
 
-class UserFormContainer extends React.Component {
-  submit = (values) => {
-    console.log(values);
-  }
-
-  render() {
-    return (
-      <UserForm onSubmit={this.submit} />
-    );
-  }
-}
-
-UserForm = reduxForm({
-  form: 'user'
-})(UserForm);
-
-export default UserFormContainer;
+export default App;
