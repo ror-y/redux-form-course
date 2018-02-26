29069b9b5403fdb29fecee22629cf0f6d4486640
Rory Smith <rory.smith@korelogic.co.uk>
2018-02-26 21:49:11 +0000

    Fix missing

---
+++ src/components/RegisterForm.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class RegisterForm extends Component {
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name</label>
+          <Field name="name" component="input" type="text" />
+        </div>
+        <div>
+          <label>Preferred Formatting</label>
+          <Field name="preference" component="select">
+            <option />
+            <option value="tabs">Tabs</option>
+            <option value="spaces">Spaces</option>
+          </Field>
+        </div>
+        <div>
+          <label>Sign up to newsletter?</label>
+          <Field name="newsletter" component="input" type="checkbox" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+RegisterForm = reduxForm({
+  form: 'user'
+})(RegisterForm);
+
+export default RegisterForm;
