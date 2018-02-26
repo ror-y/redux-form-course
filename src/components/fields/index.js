664999326e115ccc96f690188cdbdf9b2145906a
Rory Smith <rory.smith@korelogic.co.uk>
2018-02-26 21:39:18 +0000

    Add new structure

---
+++ src/components/fields/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const customInput = (props) => {
+  return (
+    <div>
+      <label>{props.label}</label>
+      <input {...props.input} type={props.type} />
+    </div>
+  )
+}
+
+export const customSelect = (props) => {
+  return (
+    <div>
+      <label>{props.label}</label>
+      <select {...props.input}>
+        <option />
+        <option value="tabs">Tabs</option>
+        <option value="spaces">Spaces</option>
+      </select>
+    </div>
+  )
+}
