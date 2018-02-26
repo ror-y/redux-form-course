3fb5427912c7a8900c3ad3c4a1e8578c138697fe
Rory Smith <rory.smith@korelogic.co.uk>
2018-02-26 21:33:36 +0000

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,55 +1,5 @@
-import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
-
-const customInput = (props) => {
-  return (
-    <div>
-      <label>{props.label}</label>
-      <input {...props.input} type={props.type} />
-    </div>
-  )
-}
-
-const customSelect = (props) => {
-  return (
-    <div>
-      <label>{props.label}</label>
-      <select {...props.input}>
-        <option />
-        <option value="tabs">Tabs</option>
-        <option value="spaces">Spaces</option>
-      </select>
-    </div>
-  )
-}
-
-class UserForm extends Component {
-  render() {
-    const { handleSubmit } = this.props;
-    return (
-      <form onSubmit={handleSubmit}>
-        <Field
-          name="name"
-          component={customInput}
-          type="text"
-          label="Name"
-        />
-        <Field
-          name="preference"
-          component={customSelect}
-          label="Preferred Formatting"
-        />
-        <Field
-          name="newsletter"
-          component={customInput}
-          type="checkbox"
-          label="Sign up to Newsletter?"
-        />
-        <button type="submit">Submit</button>
-      </form>
-    );
-  }
-}
+import React from 'react';
+import UserForm from './components/UserForm';
 
 class UserFormContainer extends React.Component {
   submit = (values) => {
@@ -62,9 +12,5 @@ class UserFormContainer extends React.Component {
     );
   }
 }
-
-UserForm = reduxForm({
-  form: 'user'
-})(UserForm);
 
 export default UserFormContainer;
