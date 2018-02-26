4aee5e7217f38edefdbaa1c9343213c05dd383fe
Rory Smith <rory.smith@korelogic.co.uk>
2018-02-26 22:18:37 +0000

    Add react json

--- src/components/fields/index.js
+++ src/components/fields/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ReactJson from 'react-json-view';
 
 export const customInput = (props) => {
   return (
     <div>
       <label>{props.label}</label>
       <input {...props.input} type={props.type} />
+      <ReactJson src={props.meta} />
     </div>
   )
 }
@@ -18,6 +20,7 @@ export const customSelect = (props) => {
         <option value="tabs">Tabs</option>
         <option value="spaces">Spaces</option>
       </select>
+      <ReactJson src={props.meta} />
     </div>
   )
 }
