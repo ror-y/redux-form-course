e3a1fea8d47ffd526ecd3a3c842304145afd09d9
Rory Smith <rory.smith@korelogic.co.uk>
2018-03-03 12:48:30 +0000

    Add normalize for names

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { customInput, customSelect } from './fields';
+import capitalize from 'capitalize';
 import {
   required,
   minLength,
@@ -21,6 +22,7 @@ class RegisterForm extends Component {
           type="text"
           label="First Name"
           validate={[required]}
+          normalize={capitalize}
         />
         <Field
           name="surname"
@@ -28,6 +30,7 @@ class RegisterForm extends Component {
           type="text"
           label="Surname"
           validate={[required]}
+          normalize={capitalize}
         />
         <Field
           name="username"
