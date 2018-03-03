b6f49ce14ab7abfbcf48b2cd5877200c77ae5dd1
Rory Smith <rory.smith@korelogic.co.uk>
2018-03-03 22:14:15 +0000

    Fix formatting

--- src/validation/index.js
+++ src/validation/index.js
@@ -1,25 +1,23 @@
-export const required = value => (
-  value ? undefined : "Value is required"
-);
+export const required = value =>
+  value ? undefined : 'Value is required';
 
-export const minLength = value => (
-  value.length < 4 ? 'Value must be at least 4 characters' : undefined
-);
+export const minLength = value =>
+  value.length < 4
+    ? 'Value must be at least 4 characters'
+    : undefined;
 
-export const maxLength = value => (
-  value.length > 10 ? 'Value is too long' : undefined
-);
+export const maxLength = value =>
+  value.length > 10 ? 'Value is too long' : undefined;
 
-export const matchesPassword = (value, allValues) => (
-  value === allValues.password ? undefined : 'Passwords must match'
-);
+export const matchesPassword = (value, allValues) =>
+  value === allValues.password ? undefined : 'Passwords must match';
 
-export const asyncValidate = (values) => {
-  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+export const asyncValidate = values => {
+  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
   return sleep(1000).then(() => {
     if (['kent', 'andy', 'john', 'joel'].includes(values.username)) {
       return Promise.reject({
-        username: 'Username already taken',
+        username: 'Username already taken'
       });
     }
   });
