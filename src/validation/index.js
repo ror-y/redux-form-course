4f59691c318aaa4dc32ccf31e04c435ea0adedf2
Rory Smith <rory.smith@korelogic.co.uk>
2018-03-03 22:16:23 +0000

    Fix formatting

--- src/validation/index.js
+++ src/validation/index.js
@@ -1,31 +1,31 @@
 export const validate = values => {
-  const errors = {}
+  const errors = {};
 
   if (!values.firstname) {
-    errors.firstname = "First name is required"
+    errors.firstname = 'First name is required';
   }
 
   if (!values.surname) {
-    errors.surname = "Surname is required"
+    errors.surname = 'Surname is required';
   }
 
   if (!values.username) {
-    errors.username = "Username is required"
+    errors.username = 'Username is required';
   } else if (values.username.length < 4) {
-    errors.username = "Username must be at least 4 characters long"
+    errors.username = 'Username must be at least 4 characters long';
   } else if (values.username.length > 10) {
-    errors.username = "Username is too long"
+    errors.username = 'Username is too long';
   }
 
-  return errors
-}
+  return errors;
+};
 
 export const warn = values => {
-  const warnings = {}
+  const warnings = {};
 
   if (/\W/g.test(values.firstname)) {
-    warnings.firstname = "Non-word character typed. Made a typo?"
+    warnings.firstname = 'Non-word character typed. Made a typo?';
   }
 
-  return warnings
-}
+  return warnings;
+};
