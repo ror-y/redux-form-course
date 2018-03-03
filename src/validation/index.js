42e5e2f583d57c2c49fda315b4299178b8b075fd
Rory Smith <rory.smith@korelogic.co.uk>
2018-03-03 22:16:13 +0000

    Fix formatting

--- src/validation/index.js
+++ src/validation/index.js
@@ -1,15 +1,15 @@
-export const required = value => (
-  value ? undefined : "Value is required"
-);
+export const required = value =>
+  value ? undefined : 'Value is required';
 
-export const minLength = value => (
-  value.length < 4 ? 'Value must be at least 4 characters' : undefined
-);
+export const minLength = value =>
+  value.length < 4
+    ? 'Value must be at least 4 characters'
+    : undefined;
 
-export const maxLength = value => (
-  value.length > 10 ? 'Value is too long' : undefined
-);
+export const maxLength = value =>
+  value.length > 10 ? 'Value is too long' : undefined;
 
-export const nonWordCharacters = value => (
-  /\W/g.test(value) ? "Non-word character typed. Made a typo?" : undefined
-);
+export const nonWordCharacters = value =>
+  /\W/g.test(value)
+    ? 'Non-word character typed. Made a typo?'
+    : undefined;
