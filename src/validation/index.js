eaf3be86e963a3f5603c8dff8b93ca183009d791
Rory Smith <rory.smith@korelogic.co.uk>
2018-03-03 22:14:54 +0000

    Fix formatting

--- src/validation/index.js
+++ src/validation/index.js
@@ -1,26 +1,24 @@
 import { SubmissionError } from 'redux-form';
 
-export const required = value => (
-  value ? undefined : "Value is required"
-);
+export const required = value =>
+  value ? undefined : 'Value is required';
 
-export const minLength = value => (
-  value.length < 4 ? 'Value must be at least 4 characters' : undefined
-);
+export const minLength = value =>
+  value.length < 4
+    ? 'Value must be at least 4 characters'
+    : undefined;
 
-export const maxLength = value => (
-  value.length > 10 ? 'Value is too long' : undefined
-);
+export const maxLength = value =>
+  value.length > 10 ? 'Value is too long' : undefined;
 
-export const matchesPassword = (value, allValues) => (
-  value === allValues.password ? undefined : 'Passwords must match'
-);
+export const matchesPassword = (value, allValues) =>
+  value === allValues.password ? undefined : 'Passwords must match';
 
-export const asyncUsernameCheck = (values) => {
+export const asyncUsernameCheck = values => {
   if (['kent', 'andy', 'john', 'joel'].includes(values.username)) {
     throw new SubmissionError({
       username: 'Username already taken',
       _error: 'Login failed!'
-    })
+    });
   }
-}
+};
