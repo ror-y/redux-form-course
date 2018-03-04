5229bf51c6cc3cd04482e4d9ae93dc46af36031d
Rory Smith <rory.smith@korelogic.co.uk>
2018-03-04 20:27:09 +0000

    Fix devtools

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ const reducers = {
 const reducer = combineReducers(reducers);
 let store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(
